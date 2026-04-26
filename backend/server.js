6c922f8b61d7424451dfbb46c1b5e6f8a16acc9c
Sindikasi <admin@spdindonesia.org>
2026-04-26 13:02:30 +0700

    fix: remove corrupted duplicate shutdown lines

--- backend/server.js
+++ backend/server.js
@@ -232,5 +232,3 @@ function shutdown(signal) {
 }
 process.on('SIGTERM', () => shutdown('SIGTERM'));
 process.on('SIGINT',  () => shutdown('SIGINT'));
-shutdown('SIGTERM'));
-process.on('SIGINT',  () => shutdown('SIGINT'));
